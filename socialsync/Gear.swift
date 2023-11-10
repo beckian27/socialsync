f0d9871ee966fed8a72ebc8c4f70f43a242a9702
Yuansong W <120421889+AW706@users.noreply.github.com>
2023-11-09 22:22:53 -0500

    Update Gear.swift

--- socialsync/Gear.swift
+++ socialsync/Gear.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Yuansong Wang on 11/1/23.
 //
-
+//On line 53-55, use the api to get an array of Date Interval to initialize possible_time with, and make these three variables @State var inside the Gear: View
+//On line 275, get the list_time as an array of Date Interval to the API
 import SwiftUI
 import UIKit
 func change_back(list: [RectangleView], num: Int) -> [RectangleView]{
@@ -25,10 +26,10 @@ func change_color(list: [RectangleView], num: Int) -> [RectangleView]{
 }
 func createRectangles(num: Int) -> [RectangleView]{
     var rects: [RectangleView] = []
-    for _ in 0...num{
-        let temp1 = 400.0 / Double(num) - 2.0
-        var temp2 = Double(rects.count) * 420.0 / Double(num)
-        temp2 -= 200 - temp1/2
+    for i in 0..<num{
+        let temp1 = (360.0 - 2.0 * Double(num)) / Double(num)
+        var temp2 = Double(i) * (temp1 + 2) + 10
+        temp2 -= 190 - temp1/2
         rects.append(RectangleView(id: rects.count + 1, color: Color.red, width: temp1, offset: temp2))
     }
     return rects;
@@ -42,11 +43,43 @@ func reset_color(rect: [RectangleView], num: Int) -> [RectangleView]{
     }
     return temp
 }
+
+//example date intervals for testing
+let currentDate = Date()
+let interval1 = DateInterval(start: currentDate, duration: 10800) // 3 hour
+let interval2 = DateInterval(start: currentDate.addingTimeInterval(3600*24), end: currentDate.addingTimeInterval(3600*32)) // 1 hour from now to 2 hours from now
+//let interval3 = DateInterval(start: currentDate.addingTimeInterval(7200), duration: 3600) // 1 hour from 2 hours from now
+
+let possible_time: [DateInterval] = [interval1, interval2] //call api here to get the possible time and make it a state var in Gear: view
+var current_index_on_possible_time = 0 //which time interval we are on
+var list_time: [DateInterval] = [] //our return list of availabilities
+func get_proper_length(x: DateInterval) -> (Double, Int){
+    let durationInSeconds = x.duration
+    let durationInHours = durationInSeconds / 3600
+    if durationInHours >= 10{
+        return (durationInHours / 2, 7200) //two hours
+    }else if durationInHours < 5 {
+        return (durationInHours * 2, 1800) //half an hour
+    }
+    return (durationInHours, 3600) //exactly one hour
+}
+func date_to_date(x: Date) -> String{
+    let df = DateFormatter()
+    df.dateFormat = "MM/dd/yyyy"
+    return df.string(from: x)
+}
+func date_to_string(x: Date) -> String{
+    let df = DateFormatter()
+    df.dateFormat = "HH:mm"
+    return df.string(from: x)
+}
 struct Gear: View {
     var gears = Image("gear")
     var pointer = Image("pointer")
-    @State var pp: Double = -190.0
-    @State var rectangles : [RectangleView] = createRectangles(num: 5)
+    @State var pp: Double = -180.0
+    @State var num_rect = Int(get_proper_length(x: possible_time[current_index_on_possible_time]).0)
+    @State var time_seg = get_proper_length(x: possible_time[current_index_on_possible_time]).1
+    @State var rectangles : [RectangleView] = createRectangles(num: Int(get_proper_length(x: possible_time[current_index_on_possible_time]).0))
     @State var green_bar = 0.0
     @State var last_false: Double = 0.0
     @Environment(\.presentationMode) var presentationMode
@@ -57,17 +90,17 @@ struct Gear: View {
     @State var barv = 0.0
     @State var gap = 0.0
     @State var loops = 0.0
+    @State var speed_transform = 100.0
     @State private var isATriggered = false
     @State private var isBTriggered = false
-    @State var date = "10/27/2023"
+    @State var date = date_to_date(x: possible_time[current_index_on_possible_time].start)
     var rect1 = CGRect(x: 20, y: 20, width: 100, height: 100)
-    @State var once:Bool = false
-
-    
+//    @State var once:Bool = false
     var body: some View {
-        Text("Available")
-            
-            .foregroundColor(Color.green)
+//        Text("Available")
+//            
+        
+//            .foregroundColor(Color.green)
                 gears
                     .resizable()
                     .frame(width: 250, height: 250)
@@ -128,10 +161,13 @@ struct Gear: View {
                                 barv += diff
                                 rotation = abs(temp)
                                 green_bar = barv
-                                pp = -190 + barv/(2*Double.pi)*70
-                                gap = 460.0 - rectangles[0].width * Double(rectangles.count)
-                                print(gap)
-                                gap /= -(Double(rectangles.count) - 1.0)
+                                if rectangles.count > 8{
+                                    speed_transform = 50.0
+                                }
+                                pp = -180 + barv/(2*Double.pi)*speed_transform
+                                gap = 1
+//                                print(gap)
+                                //gap /= -(Double(rectangles.count) - 1.0)
                                 gap *= Double(current_block)
                                 if (pp + 190.0) > Double((Double(current_block) + 1.0) * rectangles[0].width + gap ) {
                                     if avil{
@@ -140,8 +176,8 @@ struct Gear: View {
                                     current_block += 1
                                 }
 //                                print(pp)
-                                if pp > 200 {
-                                    pp = 200
+                                if pp > 180 {
+                                    pp = 180
                                 }
                             }
                             .onEnded{ value in
@@ -169,40 +205,48 @@ struct Gear: View {
 //                    .foregroundColor(Color.green)
 //                    .cornerRadius(10)
 //                    .offset(x:-190, y:-61)
-                Text("9:00      9:30       10:00        10:30         11:00     11:30")
-                    .offset(x:0,y:30)
+//                Text("9:00      9:30       10:00        10:30         11:00     11:30")
+//                    .offset(x:0,y:30)
                 ZStack {
                     Text(date)
                         .offset(x:0,y:10)
-                    ForEach(0 ..< rectangles.count) {id in
-                        @State var temp = rectangles[id].color
+                    ForEach(0 ..< rectangles.count, id:\.self) {id in
                         Rectangle()
-                            .frame(width: rectangles[id].width, height: 20)
-                            //.cornerRadius(10)
-                            .offset(x: rectangles[id].offset, y: -40) // Offset the Rectangle up
-                            .zIndex(1)
+                            .frame(width: rectangles[id].width, height: 30)                            
+                            .offset(x: rectangles[id].offset, y: -135)
+                            .foregroundColor(rectangles[id].color)
                             .onTapGesture{
-                                print("yes")
                                 rectangles = reset_color(rect: rectangles, num: id)
-                                temp = Color.green
                             }
-                            .foregroundColor(temp)
+                        
+                        
                     }
                     
+                        //the last time mark
+                    ForEach(0..<num_rect) {id in
+                        Text(date_to_string(x: possible_time[current_index_on_possible_time].start.addingTimeInterval(Double(time_seg) * Double(id))))
+                            .offset(x: rectangles[id].offset - rectangles[id].width/2, y: -110)
+                            .font(.system(size: 12))
+                        
+                    }
+                    //Text(possible_time[current_index_on_possible_time].start)//
+                        Text(date_to_string(x: possible_time[current_index_on_possible_time].start.addingTimeInterval(Double(time_seg) * Double(num_rect))))
+                            .offset(x: rectangles[rectangles.count-1].offset + rectangles[rectangles.count-1].width/2, y: -110)
+                            .font(.system(size: 12))
                     pointer
                         .resizable()
                                     .scaledToFit() // Maintain the aspect ratio of the image
                                     .scaleEffect(0.07) // Adjust the scaling factor as needed
                                     
-                                    .offset(x: pp, y:  -65)
+                                    .offset(x: pp, y:  -165)
 //                        .resizable()
 //                        .frame(width: 50, height: 50)
 //                    Text("Unavailable")
 //                        .foregroundColor(Color.red)
 //                        .offset( x:-150, y: -105)
-                    Slider(value: $barv, in: 0...10*Double.pi)
-                        .zIndex(100)
-                        .offset(y: -78 )
+//                    Slider(value: $barv, in: 0...10*Double.pi)
+//                        .zIndex(100)
+//                        .offset(y: -78 )
 //                        .onChange(of: barv) { newValue in
 //                            if newValue - loops * 2 * Double.pi > 2 * Double.pi{
 //                                loops += 1
@@ -214,13 +258,23 @@ struct Gear: View {
 //                           }
                     Button(action: {
                         print("Submitted")
-
+                        for id in 0..<rectangles.count{
+                            if rectangles[id].color == Color.green{
+                                //print(current_index_on_possible_time)
+                                list_time.append(DateInterval(start: possible_time[current_index_on_possible_time].start.addingTimeInterval(Double(time_seg) * Double(id)), duration: Double(time_seg)))
+                            }
+                        }
+                        //print(date_to_string(x:list_time[0].start))
                         //pass to api to delete the invite
-                        if !once {
-                            once = true
-                            date = "10/28/2023"
+                        if current_index_on_possible_time < (possible_time.count - 1) {
+                            let dateFormatter = DateFormatter()
+                            dateFormatter.dateFormat = "MM/dd/yyyy"
+                            date = dateFormatter.string(from: possible_time[current_index_on_possible_time].start)
+                            current_index_on_possible_time += 1
                         }else{
-                            let _:[String] = apipost(endpoint: "/test/1/", parameters: [:])
+                            //let _:[String] = apipost(endpoint: "/test/1/", parameters: [:])
+                            // feed the list_time back to the api.
+                            current_index_on_possible_time = 0
                             presentationMode.wrappedValue.dismiss()
 
                         }
